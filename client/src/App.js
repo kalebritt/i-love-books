0855fd8d71b437f5da178aff35a33dbbbe25a76d
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-11-19 11:27:27 -0700

    refactoring and adding goodies

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+//import other goodies
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+//import more apollo goodies
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
@@ -10,9 +18,9 @@ function App() {
       <>
         <Navbar />
         <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          <Route exact path="/" component={SearchBooks} />
+          <Route exact path="/saved" component={SavedBooks} />
+          <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
         </Switch>
       </>
     </Router>
