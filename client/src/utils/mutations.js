23c44296fc7bb078debc88b0bdc9c2b45157bb54
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-11-19 11:05:07 -0700

    added login_user

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,2 +1,15 @@
 //import apollo/client
 import { gql } from "@apollo/client";
+
+//export login_user
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
