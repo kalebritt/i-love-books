0156ad197678404d6be7feb549514d28920e0a4b
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-11-19 10:12:48 -0700

    added resolvers

--- server/controllers/schemas/resolvers.js
+++ server/controllers/schemas/resolvers.js
@@ -2,3 +2,18 @@
 const { AuthenticationError } = require("apollo-server-express");
 const { User } = require("/server/models");
 const { signToken } = require("/server/utils");
+
+//resolvers
+const resolvers = {
+    Query: {
+      me: async (parent, args, context) => {
+        if (context.user) {
+          const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+          return userData;
+        }
+        throw new AuthenticationError('You need to be logged in!');
+      },
+    },
+
+    //mutation
+
