0be133d63c8e25f272ef62204e23946d00226bf4
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-11-19 10:32:36 -0700

    added typeDefs and type Auth

--- server/controllers/schemas/typeDefs.js
+++ server/controllers/schemas/typeDefs.js
@@ -1,4 +1,18 @@
 //gql apollo
-const { gql } = require("apollo-server-express")
+const { gql } = require("apollo-server-express");
 
 //typeDefs = gql
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  # type Auth
+  type Auth {
+    token: ID!
+    user: User
+  }
+`;
